34f0f631d0f778d03b5ea4815259cea566de83f1
Gaballa <lGaballal@yahoo.com>
2026-06-30 17:26:15 -0700

    quick note

--- shells/go-api.nix
+++ shells/go-api.nix
@@ -2,6 +2,10 @@
   pkgs,
   db,
 }: let
+  # The snowflake driver is being pulled into the dependencies
+  # and throws a panic because of CA cert failing when initialized.
+  # So to get around this I override the build tag to
+  # only build for one specified db
   db-migrate = pkgs.go-migrate.overrideAttrs (old: {
     tags = ["${db}"];
   });
